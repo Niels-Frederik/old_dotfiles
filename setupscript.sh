0f40c063fdb9d8f2ccff97fb304fc32e0b086b37
Niels-Frederik Brinck <niels.frederik.brinck@gmail.com>
2021-02-06 18:38:57 +0100

    added font install and copy of dotfiles from this folder to user. Started adding support for setting up polybar

--- setupscript.sh
+++ setupscript.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-sudo pacman -Syu -y
-sudo pacman -S chromium -y
-sudo pacman -S rofi -y
-sudo rm -rf /usr/share/conky
-sudo rm -rf /usr/bin/start_conkey_grey
-sudo rm -rf /usr/bin/start_conkey_green
-sudo rm -rf /usr/bin/start_conkey_live_green
-sudo rm -rf /usr/bin/start_conkey_live_grey
-sudo rm -rf /usr/bin/start_conkey_live_maia
-sudo rm -rf /usr/bin/start_conkey_live_solarized
-sudo rm -rf /usr/bin/start_conkey_maia
-sudo rm -rf /usr/bin/start_conkey_solarized
-#yay iosevka option 2
+sudo pacman -Syu --noconfirm
+sudo pacman -S chromium rofi polybar --noconfirm 
+sudo rm -rf ~/usr/share/conky
+sudo rm -rf ~/usr/bin/start_conkey_grey
+sudo rm -rf ~/usr/bin/start_conkey_green
+sudo rm -rf ~/usr/bin/start_conkey_live_green
+sudo rm -rf ~/usr/bin/start_conkey_live_grey
+sudo rm -rf ~/usr/bin/start_conkey_live_maia
+sudo rm -rf ~/usr/bin/start_conkey_live_solarized
+sudo rm -rf ~/usr/bin/start_conkey_maia
+sudo rm -rf ~/usr/bin/start_conkey_solarized
+yay -S ttf-iosevka --removemake --nocleanmenu --nodiffmenu --noeditmenu
+yes | cp .Xresources ~
+yes | cp config ~/.i3/
+
+sudo mkdir ~/.config/polybar
+sudo cp /usr/share/doc/polybar/config ~/.config/polybar
+sudo chown "$USER:$USER" ~/.config/polybar/config
