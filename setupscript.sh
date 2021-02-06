08e43486030478447820a1df7568f2bba2836a00
Niels-Frederik Brinck <niels.frederik.brinck@gmail.com>
2021-02-06 16:53:40 +0100

    Added Xresources default file

--- setupscript.sh
+++ setupscript.sh
@@ -2,4 +2,13 @@
 sudo pacman -Syu -y
 sudo pacman -S chromium -y
 sudo pacman -S rofi -y
-
+sudo rm -rf /usr/share/conky
+sudo rm -rf /usr/bin/start_conkey_grey
+sudo rm -rf /usr/bin/start_conkey_green
+sudo rm -rf /usr/bin/start_conkey_live_green
+sudo rm -rf /usr/bin/start_conkey_live_grey
+sudo rm -rf /usr/bin/start_conkey_live_maia
+sudo rm -rf /usr/bin/start_conkey_live_solarized
+sudo rm -rf /usr/bin/start_conkey_maia
+sudo rm -rf /usr/bin/start_conkey_solarized
+#yay iosevka option 2
