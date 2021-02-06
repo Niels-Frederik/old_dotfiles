ed7a88f79588c43262d428bfbf01d24f42e40df6
Niels-Frederik Brinck <niels.frederik.brinck@gmail.com>
2021-02-06 19:33:52 +0100

    scripts setup with polybar install and configuration

---
+++ load_user_files_into_this_dir.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+yes | cp ~/.i3/config .
+yes | cp ~/.Xresources .
+yes | cp ~/.config/polybar/polybar_launch.sh ./polybar 
+yes | cp ~/.config/polybar/config ./polybar
--- setupscript.sh
+++ setupscript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+echo "Updating system and installing packages"
 sudo pacman -Syu --noconfirm
 sudo pacman -S chromium rofi polybar --noconfirm 
+yay -S ttf-iosevka --removemake --nocleanmenu --nodiffmenu --noeditmenu
+echo "Removing default conky widgets on desktop"
 sudo rm -rf ~/usr/share/conky
 sudo rm -rf ~/usr/bin/start_conkey_grey
 sudo rm -rf ~/usr/bin/start_conkey_green
@@ -10,10 +13,12 @@ sudo rm -rf ~/usr/bin/start_conkey_live_maia
 sudo rm -rf ~/usr/bin/start_conkey_live_solarized
 sudo rm -rf ~/usr/bin/start_conkey_maia
 sudo rm -rf ~/usr/bin/start_conkey_solarized
-yay -S ttf-iosevka --removemake --nocleanmenu --nodiffmenu --noeditmenu
+echo "Moving files from github repository into this setup"
 yes | cp .Xresources ~
 yes | cp config ~/.i3/
+sudo rm -rf ~/.config/polybar
+mkdir ~/.config/polybar
+cp ./polybar/config ~/.config/polybar/
+#sudo chown "$USER:$USER" ~/.config/polybar/config
+yes | cp ./polybar/polybar_launch.sh ~/.config/polybar/
 
-sudo mkdir ~/.config/polybar
-sudo cp /usr/share/doc/polybar/config ~/.config/polybar
-sudo chown "$USER:$USER" ~/.config/polybar/config
